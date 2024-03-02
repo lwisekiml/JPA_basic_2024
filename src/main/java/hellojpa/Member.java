40c7ee296b3cb1321e48fea93237cd06c5a2ec4b
wisekim <lwisekiml@naver.com>
2024-03-02 18:21:12 +0900

    기본 키 매핑 - 시퀀스 전략

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,9 +3,10 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
     private Long id;
 
     @Column(name = "name", nullable = false)
