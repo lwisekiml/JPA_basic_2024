eba2bab8d4f4bca72bed060347b87a4d4a9fb76c
wisekim <lwisekiml@naver.com>
2024-03-04 19:19:02 +0900

    다대일 [N:1] : 단방향

--- hello-jpa/src/main/java/hellojpa/Member.java
+++ hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,15 +4,13 @@ import jakarta.persistence.*;
 
 @Entity
 public class Member {
+
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
     @Column(name = "USERNAME")
     private String username;
-
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
 
     @ManyToOne//(fetch = FetchType.LAZY) // 없으면 한번에 join해서 가져온다.
     @JoinColumn(name = "TEAM_ID") // join해야 되는 컬럼이 뭐냐
@@ -40,11 +38,6 @@ public class Member {
 
     public void setTeam(Team team) {
         this.team = team;
-    }
-
-    public void changeTeam(Team team) { // 연관 관계 편의 메소드를 사용해도 된다.
-        this.team = team;
-        team.getMembers().add(this);
     }
 
     @Override
--- hello-jpa/src/main/java/hellojpa/Team.java
+++ hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import jakarta.persistence.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Team {
@@ -12,14 +12,6 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
-
-    @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>();
-
-    public void addMember(Member member) {
-        member.setTeam(this);
-        members.add(member);
-    }
 
     public Long getId() {
         return id;
@@ -35,22 +27,5 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-
-    @Override
-    public String toString() {
-        return "Team{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", members=" + members +
-                '}';
     }
 }
