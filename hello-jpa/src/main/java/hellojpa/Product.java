c2161a36a02761c101c48ca68ff7f92c5a1e17a7
wisekim <lwisekiml@naver.com>
2024-03-05 11:07:51 +0900

    다대다 [N:M] : 양방향
    - 다대다는 사용x

--- hello-jpa/src/main/java/hellojpa/Product.java
+++ hello-jpa/src/main/java/hellojpa/Product.java
@@ -3,6 +3,10 @@ package hellojpa;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -11,6 +15,9 @@ public class Product {
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
