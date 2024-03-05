4572962a989a866592a6c60336f01db758df89df
wisekim <lwisekiml@naver.com>
2024-03-05 21:59:06 +0900

    Mapped Superclass - 매핑 정보 상속
    - BaseEntity는 Member, Team이 공통으로 사용하는 매핑 정보를 모으는 역할(주로 등록일, 수정일 등 사용)
    - 상속관계 매핑, 엔티티, 테이블과 매핑, 조회, 검색 모두 X, 불가
    - BaseEntity는 직접 생성해서 사용할 일이 없으므로 추상클래스 권장

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDateTime;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,22 +19,15 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("aaa");
-            movie.setActor("bbbb");
-            movie.setName("바람과함계사라지다");
-            movie.setPrice(10000);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            em.persist(movie); // insert 2번(item, movie)
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            // 이 경우 union을 사용해서 복잡한 쿼리가 나가게 된다.
-            // item id만 아는 경우, 테이블 3개를 다 select 해봐야 알 수 있다.
-            // 비효율적으로 동작
-            Item item = em.find(Movie.class, movie.getId());
-            System.out.println("item = " + item);
 
             tx.commit();
         } catch (Exception e) {
