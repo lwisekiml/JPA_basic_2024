0f2b75430f4a0faa6618f4762219dd9c8be5da32
wisekim <lwisekiml@naver.com>
2024-03-06 21:55:16 +0900

    프록시 : find() 비교

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,35 +19,23 @@ public class JpaMain {
 
         try {
 
-//            Member member = em.find(Member.class, 1L);
-//
-//            printerMember(member);
-//
-//            printMemberAndTeam(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("hello");
-
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            //
-//            Member findMember = em.find(Member.class, member.getId()); // join 을 해서 가져온다.
-            Member findMember = em.getReference(Member.class, member.getId());
-            // 프록시 객체 findMember = class hellojpa.Member$HibernateProxy$tx5DdrBv
-            System.out.println("before findMember = " + findMember.getClass());
-            // 위에 getId()해서 가져온것이 있어 쿼리 없음
-            System.out.println("findMember.id = " + findMember.getId());
-            // 쿼리 날림
-            // 프록시 객체는 처음 사용할 때 한 번만 초기화
-            // 프록시 객체를 초기화할 때 프록시 객체가 실제 엔티티로 바뀌는 것은 아니다.(before/after 확인)
-            // 초기화되면 프록시 객체를 통해 실제 엔티티 접근 가능
-            System.out.println("findMember.username = " + findMember.getUsername());
-            // 쿼리x  (프록시 객체를 통해 실제 엔티티 접근)
-            System.out.println("findMember.username = " + findMember.getUsername());
-            System.out.println("after findMember = " + findMember.getClass());
+            Member m1 = em.find(Member.class, member1.getId()); // Member
+            Member m2 = em.find(Member.class, member2.getId()); // Member
+
+            System.out.println("m1 = " + m1.getClass()); // m1 = class hellojpa.Member
+            System.out.println("m2 = " + m2.getClass()); // m2 = class hellojpa.Member
+            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
 
             tx.commit();
         } catch (Exception e) {
