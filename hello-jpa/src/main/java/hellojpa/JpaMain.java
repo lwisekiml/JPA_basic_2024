cd756eb6451293ecc4b522af510bc536bba9c722
wisekim <lwisekiml@naver.com>
2024-03-06 22:02:48 +0900

    프록시 : find() 뒤에 getReference()하는 경우

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -30,13 +30,21 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Member m1 = em.find(Member.class, member1.getId()); // Member
-            Member m2 = em.getReference(Member.class, member2.getId()); // Proxy
+            System.out.println("=================1=================");
+            Member m1 = em.find(Member.class, member1.getId());
+            System.out.println("=================2=================");
+            Member m2 = em.find(Member.class, member2.getId());
+            System.out.println("=================3=================");
+            // 2번때문에 이미 영속성 컨텍스트에 있기 때문에 m2와 같은 member가 된다.
+            Member m3 = em.getReference(Member.class, member2.getId());
+            System.out.println("==================================");
+            // 2번을 getReference()로 하고 m3을 find()로 하면 m2, m3은 Proxy가 된다.
 
             System.out.println("m1 = " + m1.getClass()); // m1 = class hellojpa.Member
-            System.out.println("m2 = " + m2.getClass()); // m2 = class hellojpa.Member$HibernateProxy$bHhvfrfj
-            System.out.println("m1 : " + (m1 instanceof Member)); // false
-            System.out.println("m2 : " + (m2 instanceof Member)); // false
+            System.out.println("m2 = " + m2.getClass()); // m2 = class hellojpa.Member
+            System.out.println("m3 = " + m3.getClass()); // m3 = class hellojpa.Member
+            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); // true
+            System.out.println("m1 == m3 : " + (m1.getClass() == m3.getClass())); // true
 
             tx.commit();
         } catch (Exception e) {
