39b8109ce5e174909877227a5a3258454ab89f5b
wisekim <lwisekiml@naver.com>
2024-03-12 21:44:40 +0900

    값 타입 컬렉션 : 조회 예제
    - 값 타입 컬렉션도 지연 로딩 전략 사용

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.List;
+import java.util.Set;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -17,15 +20,35 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Address address = new Address("city", "street", "10000");
-//
-//            Member member = new Member();
-//            member.setUsername("member1");
-//            member.setHomeAddress(address);
-//            em.persist(member);
-//
-//            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-//            member.setHomeAddress(newAddress);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            System.out.println("============== START ==============");
+            Member findMember = em.find(Member.class, member.getId()); // member만 가져온다. 컬렉션은 다 지연로딩이다.
+
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address = " + address.getCity());
+            }
+
+            Set<String> favoriteFoods = findMember.getFavoriteFoods();
+            for (String favoriteFood : favoriteFoods) {
+                System.out.println("favoriteFood = " + favoriteFood);
+            }
 
             tx.commit();
         } catch (Exception e) {
--- hello-jpa/src/main/java/hellojpa/Member.java
+++ hello-jpa/src/main/java/hellojpa/Member.java
@@ -32,4 +32,43 @@ public class Member {
         @JoinColumn(name = "MEMBER_ID"))
     private List<Address> addressHistory = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
 }
