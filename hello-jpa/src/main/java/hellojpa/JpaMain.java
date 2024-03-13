c8366b626edb06902019da9a6255538372c05213
wisekim <lwisekiml@naver.com>
2024-03-13 21:36:34 +0900

    객체지향 쿼리 언어1 - 기본 문법 소개 : Criteria
    - 사용하지 않는다.

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 import java.util.List;
 import java.util.Set;
@@ -21,14 +24,16 @@ public class JpaMain {
 
         try {
 
-            List<Member> result = em.createQuery( // Member는 entity 객체이다.
-                    "select m from Member m where m.username like '%kim'",
-                    Member.class
-            ).getResultList();
+            //Criteria 사용 준비
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            for (Member member : result) {
-                System.out.println("member = " + member);
-            }
+            //루트 클래스 (조회를 시작할 클래스)
+            Root<Member> m = query.from(Member.class);
+
+            //쿼리 생성
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
