45ce7aab1d321853bc00a4115c8c88527d5faf82
wisekim <lwisekiml@naver.com>
2024-03-14 21:01:56 +0900

    기본 문법과 쿼리 API : 초기 entity 세팅

---
+++ jpql/src/main/java/jpql/Team.java
@@ -0,0 +1,45 @@
+package jpql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
