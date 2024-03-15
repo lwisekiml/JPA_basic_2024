b7ee74b6744cbe3b92d652991f79bb528e03d46a
wisekim <lwisekiml@naver.com>
2024-03-15 14:55:12 +0900

    프로젝션(SELECT) : 임베디드 타입 프로젝션

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -26,11 +26,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 아래와 같이 하면 join을 하게되지만 아래와 같이 코드를 작성할 경우 join을 할 것이라는 것이 한눈에 안보인다.
-//            List<Team> result = em.createQuery("select m.team from  Member m", Team.class)
-//                    .getResultList();
-            // 그래서 아래와 같이 작성하여 조인이 들어가는 것을 나타내는 것이 코드를 보기에도 좋다.
-            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+            em.createQuery("select o.address from Order o", Order.class)
                     .getResultList();
 
             tx.commit();
