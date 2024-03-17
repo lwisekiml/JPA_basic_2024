34a25fdf11709cadbe802b5a24dace944252a1e3
wisekim <lwisekiml@naver.com>
2024-03-17 21:48:55 +0900

    서브 쿼리

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -32,7 +32,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join Team t on m.username = t.name";
+            // select 서브 쿼리
+            String query = "select (select avg(m1.age) from Member m1) as avgAge from Member m left join Team t on m.username = t.name";
+            // from 절의 서브 쿼리는 JPQL에서 불가능 / 조인으로 풀 수 있으면 풀어서 해결
+//            String query = "select mm.age, mm.username" +
+//                    " from (select m.age, m.username from Member m) as avgAge from Member m left join Team t on m.username = t.name";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
             System.out.println("result.size = " + result.size());
