4157e23e1a9ee6c35908b4961f6d068ba7198974
wisekim <lwisekiml@naver.com>
2024-03-20 22:38:21 +0900

    페치 조인 1 - 기본 : 페치 조인 사용 전
    - 쿼리가 총 3번 나간다.
    (처음 select문, 회원1, 회원3)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,44 +19,45 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            // 1. 상태 필드 : 경로 탐색의 끝, 탐색X / m.username
-//            String query = "select m.username from Member m";
+            String query = "select m from Member m";
 
-            // 2. 단일 값 연관 경로 : 묵시적 내부 조인, 탐색 O -> 내부 조인이 발생하게 짜면 안된다.(튜닝의 어려움)/ m.team
-//            String query = "select m.team from Member m";
-//            List<Team> result = em.createQuery(query, Team.class)
-//                    .getResultList();
-//
-//            for (Team s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            // 3. 컬렉션 값 연관 경로 : 묵시적 내부 조인 발생, 탐색 X
-            // t.members에서 .으로 다른 값을 사용 불가능 하다.
-//            String query = "select t.members from Team t";
-            // 그래서 아래와 같이 사용해야 한다.
-            String query = "select m.username from Team t join t.members m";
-
-            List<Collection> result = em.createQuery(query, Collection.class)
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+
+                // 회원 100명 -> N + 1
+            }
 
             tx.commit();
         } catch (Exception e) {
